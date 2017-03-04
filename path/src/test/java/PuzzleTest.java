6fa7a3b90626fc376802309d3f24f364fafd7632
James Hill <j.g.hill@zoho.com>
2017-03-04 20:54:51 +0000

    Added test file.

---
+++ path/src/test/java/PuzzleTest.java
@@ -0,0 +1,34 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Testes for the Puzzle.kt file.
+ * 
+ * @author James
+ */
+public class PuzzleTest {
+    
+    public PuzzleTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
